6017b2b42f9f06cde03644d048f4a9dbdbc019c4
Huda Yousif <hjdoski@gmail.com>
2019-10-14 18:36:45 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end 
+end 
+
+fido = Person.new 
+fido.talk 
+
+class Person
+  def walk
+    puts "The Person is walking"
+  end 
+end 
+
+fido = Person.new 
+fido.walk 
